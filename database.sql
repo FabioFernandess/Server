dbf5265610a565689b84ccfc12cc96709171c419
Fábio Fernandes <fabioffso@gmail.com>
2022-07-21 20:31:46 -0300

    db

--- database.sql
+++ database.sql
@@ -5,10 +5,10 @@ CREATE TABLE fresadora (id SERIAL PRIMARY KEY, nome VARCHAR, valor INT);
 CREATE TABLE analise(
    id SERIAL PRIMARY KEY,
    nome_arquivo VARCHAR(255) NOT NULL,
-   valor_padrao int,
+   valor_padrao int NOT null,
    status_analise VARCHAR(1) NOT NULL,
-   data_analise timestamp default NULL
-    id_fresadora INT,
+   data_analise timestamp NOT NULL,
+    id_fresadora INT NOT null,
    CONSTRAINT fk_fresadora
       FOREIGN KEY(id_fresadora) 
 	  REFERENCES fresadora(id)
